558522794f09f1fe242b5c93eee00070b0ec8643
Dikshant <dikshantvash12@gmail.com>
2025-06-02 16:37:04 +0530

    Create piece_classification.py

---
+++ src/piece_classification.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+# Placeholder function: always returns 'empty'
+def classify_square(image):
+    # TODO: Replace with actual model prediction
+    return 'empty'
+
+def classify_squares(squares):
+    return [classify_square(sq) for sq in squares]
